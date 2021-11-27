8a2f4a9ac6dcd2b2be084ab09b6bb3b4ce3fbf1d
toastietoes <juliejyyyang@gmail.com>
2021-11-27 15:02:32 -0500

    timer

--- thecodething.py
+++ thecodething.py
@@ -5,11 +5,11 @@ from typing import Counter
 root = Tk()
 
 
-myLabel1 = Label(root, text="Grocery Store")
+myLabel1 = Label(root, text="         Grocery Store")
 myLabel2 = Label(root, text="test")
+myLabel1.config(font = ('Helvatical bold', 20))
 
-
-myLabel1.grid(row=0, column=0)
+myLabel1.grid(row=0, column=1)
 myLabel2.grid(row=1, column=0)
 
 count = 0
@@ -17,18 +17,20 @@ count = 0
 def enterClick():
     global count
     count = count+1
-    myLabel3 = Label(root, text = count)
-    myLabel3.grid(column=2, row=1)
+    myLabel3 = Label(root, text = "Total people in store: "+str(count))
+    myLabel3.config(font = ('Helvatical bold', 20))
+    myLabel3.grid(column=0, row=3)
 
 def exitClick():
     global count
     count = count-1
-    myLabel3 = Label(root, text = count)
-    myLabel3.grid(column=2, row=1)
+    myLabel3 = Label(root, text = "Total people in store: "+str(count))
+    myLabel3.config(font = ('Helvatical bold', 20))
+    myLabel3.grid(column=0, row=3)
 
 enterButton = Button(root, text = "enter", padx=50, pady=10, command = enterClick)
-enterButton.grid(row=0,column=2)
+enterButton.grid(row=1,column=1)
 
 exitButton = Button(root,text="exit", padx = 50, pady = 10, command = exitClick)
-exitButton.grid(row=0, column=3)
+exitButton.grid(row=1, column=2)
 root.mainloop() 
---
+++ timer.py
@@ -0,0 +1,13 @@
+from timeit import default_timer as timer
+n = 0 #number of people
+
+while True:
+    i = input()
+    if i == "enter":
+        start = timer()
+        n += 1
+    elif i == "exit":
+        end = timer()
+        n -= 1
+        break
+print(end - start) #seconds
