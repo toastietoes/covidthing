0a6678317ee376c901423981c9886a08eaf0718d
toastietoes <juliejyyyang@gmail.com>
2021-11-27 14:20:59 -0500

    actual first commit

--- thecodething.py
+++ thecodething.py
@@ -1,1 +1,34 @@
-print("hello world")
+from tkinter import *
+from typing import Counter
+
+
+root = Tk()
+
+
+myLabel1 = Label(root, text="Grocery Store")
+myLabel2 = Label(root, text="test")
+
+
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
+
+count = 0
+
+def enterClick():
+    global count
+    count = count+1
+    myLabel3 = Label(root, text = count)
+    myLabel3.grid(column=2, row=1)
+
+def exitClick():
+    global count
+    count = count-1
+    myLabel3 = Label(root, text = count)
+    myLabel3.grid(column=2, row=1)
+
+enterButton = Button(root, text = "enter", padx=50, pady=10, command = enterClick)
+enterButton.grid(row=0,column=2)
+
+exitButton = Button(root,text="exit", padx = 50, pady = 10, command = exitClick)
+exitButton.grid(row=0, column=3)
+root.mainloop() 
